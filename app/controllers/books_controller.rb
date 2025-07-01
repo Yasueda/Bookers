1de606d4141decdc5909a365558dfd53ed142905
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-07-01 12:02:55 +0000

    [Finish] index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,36 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
   end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      # flash[:alert] = "errors"
+      @books = Book.all
+      render :index
+    end
+  end
+
   def edit
+  end
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    flash[:notice] = "Book was succesfully destroyed."
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
