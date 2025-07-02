b82f32a5142770d4641e80f32d402c0420cd90ba
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-07-02 02:42:22 +0000

    code cleanup

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,10 +34,10 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
+    book = Book.find(params[:id])
+    book.destroy
     flash[:notice] = "Book was succesfully destroyed."
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+    redirect_to '/books'
   end
 
   private
